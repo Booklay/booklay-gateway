ddb81c077e561a28a542c91d56e2fedd8d123c5a
opgumo <opgumo@naver.com>
2023-01-03 16:25:15 +0900

    first commit

--- src/main/java/com/nhnacademy/booklay/booklaygateway/configuration/RouterConfig.java
+++ src/main/java/com/nhnacademy/booklay/booklaygateway/configuration/RouterConfig.java
@@ -5,8 +5,6 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.UUID;
-
 @Configuration
 public class RouterConfig {
 
