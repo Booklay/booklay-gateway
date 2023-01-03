63d723763bddd88577ee63673c1f316c3f81491a
opgumo <opgumo@naver.com>
2023-01-03 16:04:41 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/booklay/booklaygateway/configuration/RouterConfig.java
@@ -0,0 +1,23 @@
+package com.nhnacademy.booklay.booklaygateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
+
+@Configuration
+public class RouterConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("get_something", r -> r.path("/coupon/**")
+                        .uri("https://comsun.shop"))
+                .route("get_route", r -> r.path("/member/**")   // localhost:8080/member
+                        .uri("https://naver.com"))                      // naver.com/member
+
+                .build();
+    }
+}
---
+++ src/test/java/com/nhnacademy/booklay/booklaygateway/BooklayGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.booklay.booklaygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklayGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
